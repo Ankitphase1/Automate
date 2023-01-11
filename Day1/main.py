1d16069e5b77c80f458256f84d6dec770a17fa97
Ankit Umare <ankituser007@gmail.com>
2023-01-11 23:07:14 +0530

    day1

---
+++ Day1/main.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+p1 = Path('files/ghi.txt')
+print(type(p1))
+
+# if p1.exists():
+#     with open(p1, 'r') as file :
+#         print(file.read())
+
+if not p1.exists():
+    with open(p1, 'w') as file:
+        file.write('Content 3')
+
+
+print(p1.name)
+print(p1.stem)
+print(p1.suffix)
+
+
+p2 = Path('files')
+print(list(p2.iterdir()))        
